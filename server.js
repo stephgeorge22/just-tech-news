c85467a03163476fb103dbb9ba3f2e6d2e7ffb6b
Stephanie George <stephaniegeorge@Stephanies-MacBook-Air.local>
2021-07-23 01:11:04 -0400

    login and logout

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,6 +21,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
